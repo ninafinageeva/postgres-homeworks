6e2b4a112011ca0b2d1e23844edf88826ffea152
ninafinageeva <finageeva.nina@mail.ru>
2024-03-09 08:33:00 +0300

    homework-1 create tables.sql

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE employees
+(
+    first_name varchar(100),
+    last_name varchar(100),
+    title varchar(100),
+    birth_date char(100),
+    notes text
+);
+
+CREATE TABLE customers
+(
+    customer_id varchar(100),
+    company_name varchar(100),
+    contact_name varchar(100)
+);
+
+CREATE TABLE orders
+(
+    order_id int PRIMARY KEY,
+    customer_id varchar(100),
+    employee_id int,
+    order_date varchar(100),
+    ship_city  varchar(100)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,27 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import psycopg2
+
+if __name__ == '__main__':
+    conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='54321')
+    cur = conn.cursor()
+    try:
+
+        sql = "COPY %s FROM STDIN WITH CSV HEADER DELIMITER AS ','"
+        reed_f = open('north_data/employees_data.csv', 'r', encoding='utf8')
+        cur.execute("truncate " + 'employees' + ";")
+#        cur.copy_expert(sql=sql % 'employees', file=reed_f)
+
+        sql = "COPY %s FROM STDIN WITH CSV HEADER DELIMITER AS ','"
+        reed_f = open('north_data/customers_data.csv', 'r', encoding='utf8')
+        cur.execute("truncate " + 'customers' + ";")
+        cur.copy_expert(sql=sql % 'customers', file=reed_f)
+
+        with open('north_data/orders_data.csv', 'r', encoding='utf-8') as f:
+            cur.execute("truncate " + 'orders' + ";")
+            cur.copy_expert("COPY orders FROM STDIN WITH CSV HEADER DELIMITER AS ','", f)
+
+    finally:
+        conn.commit()
+        cur.close()
+        conn.close()
